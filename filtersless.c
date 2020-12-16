3e74e3218af3c07ab7842639c3871e5a0c8e4675
mecacca <69664192+mecacca@users.noreply.github.com>
2020-12-16 23:28:48 +0800

    Update filtersless.c

--- filtersless.c
+++ filtersless.c
@@ -1,7 +1,7 @@
 #include "helpers.h"
 #include <math.h>
 
-// "helpers.h" is not included in this respository 
+// "helpers.h" is not included in this repository 
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
